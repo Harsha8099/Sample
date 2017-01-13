7773fe338969a29ec17564148ae55209f85be312
harsha <pamulapatisriharsha@gmail.com>
2017-01-13 13:14:13 +0530

    IT000:[Harsha]-Committing Example

--- src/com/git/example/Example2.java
+++ src/com/git/example/Example2.java
@@ -3,5 +3,6 @@ package com.git.example;
 public class Example2 {
 public static void main(String[] args) {
 	System.out.println("From Example2");
+	System.out.println("Second Statement from Example2");
 }
 }
