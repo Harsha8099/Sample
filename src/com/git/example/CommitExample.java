3c5056ebfaee88e995c5725963b93da4c6dc9afe
harsha <pamulapatisriharsha@gmail.com>
2017-01-13 13:00:59 +0530

    IT000:[Harsha]-Re-Verification about Commit with Multiple Files

--- src/com/git/example/CommitExample.java
+++ src/com/git/example/CommitExample.java
@@ -3,5 +3,6 @@ package com.git.example;
 public class CommitExample {
 public static void main(String[] args) {
 	System.out.println("From CommitExample");
+	System.out.println("Committing Multiple files in Staging");
 }
 }
--- src/com/git/example/Example.java
+++ src/com/git/example/Example.java
@@ -3,5 +3,6 @@ package com.git.example;
 public class Example {
 public static void main(String[] args) {
 	System.out.println("From Example");
+	System.out.println("Committing Multiple files in Staging");
 }
 }
